a211b52663e98fa858178170b5fb9fe730328f01
Angel Gabriel Manzano Contreras <angel.manzano1326@alumnos.udg.mx>
2024-07-21 21:06:25 -0600

    contact aesthetics

--- src/components/contact-icon/index.tsx
+++ src/components/contact-icon/index.tsx
@@ -2,16 +2,19 @@ import Image from 'next/image'
 
 import { IContactIcon } from './Icontact-icon'
 
+import styles from './contact-icon.module.css'
+
 export default function index({ link, image, name } : IContactIcon) {
+  const messageToDisplay = name.split(' ')[0]
   return (
-    <article>
+    <article className={ styles[`contact-icon`] }>
       <figure>
         <a href={ link } target='_blank'>
           <Image src={ image } alt={ name } height={100} width={100} />
         </a>
       </figure>
 
-      {/* <p>{ name }</p> */}
+      <p className={ styles[`contact-icon__message`] }>{ messageToDisplay }</p>
 
     </article>
   )
