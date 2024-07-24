1c7a94a15d208bbde3f557d69b342e7d116d53b1
Angel Gabriel Manzano Contreras <angel.manzano1326@alumnos.udg.mx>
2024-07-24 10:13:34 -0600

    navbar ready

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,9 +1,32 @@
+import Link from "next/link";
 import styles from "./header.module.css";
 
 export default function Header() {
   return (
     <header className={ styles[`header`] }>
-      <h1>Manzano</h1>
+      {/* <h1>Angel Manzano</h1> */}
+      <nav className={ styles[`header__navbar`] }>
+        <ul className={ styles[`header__list`] }>
+          <li>
+            <Link className={ styles[`header__list-item`] } href="#">About me</Link>
+          </li>
+          <li>
+            <Link className={ styles[`header__list-item`] } href="#skills">Skills</Link>
+          </li>
+          <li>
+            <Link className={ styles[`header__list-item`] } href="#experience">Experience</Link>
+          </li>
+          <li>
+            <Link className={ styles[`header__list-item`] } href="#education">Education</Link>
+          </li>
+          <li>
+            <Link className={ styles[`header__list-item`] } href="#projects">Projects</Link>
+          </li>
+          <li>
+            <Link className={ styles[`header__list-item`] } href="#contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
     </header>
   )
 }
