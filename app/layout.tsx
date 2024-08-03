cbffb14c701c27bfd59b1d189054b8504f564550
Angel Gabriel Manzano Contreras <angel.manzano1326@alumnos.udg.mx>
2024-08-03 10:24:26 -0600

    fix(layout): migrating head to metdata api (icon)

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import { Inter } from "next/font/google";
 
 import "./globals.css";
 import Header from "@/src/components/header";
-import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,12 +21,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <Head>
-        <link 
-          rel="icon" 
-          href="/favicon-am.webp" 
-        />
-      </Head>
       <body className={inter.className}>
         <main>
           <Header />
