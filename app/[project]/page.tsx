82c6d1d792855131de747dba3aac74ef016794cc
Angel Gabriel Manzano Contreras <angel.manzano1326@alumnos.udg.mx>
2024-08-03 10:47:35 -0600

    feat(project): tools section added

--- app/[project]/page.tsx
+++ app/[project]/page.tsx
@@ -3,12 +3,17 @@ import { useParams } from "next/navigation"
 
 import { projects } from "@/src/data/data"
 
+import styles from "./page.module.css"
+import Tools from "@/src/sections/(project)/tools"
+
 export default function page() {
   const { project } = useParams()
+  const projectData = projects[ project ];
 
   return (
-    <main>
+    <main className={ styles[`project-main`] }>
       <h2>{ project }</h2>
+      <Tools tools={ projectData.tools } />
     </main>
   )
 }
---
+++ src/sections/(project)/tools/index.tsx
@@ -0,0 +1,17 @@
+import styles from"./tools.module.css";
+import Skill from "@/src/components/skill";
+
+export default function index({ tools }) {
+  return (
+    <section className={ styles[`project-tools`] }>
+      <h3 className={ styles[`project-tools__title`] }>Tools used</h3>
+
+      <section className={ styles[`project-tools__container`] }>
+        {
+          tools.map((tool, index) => ( <Skill key={index} {...tool} /> ))
+        }
+      </section>
+
+    </section>
+  )
+}
