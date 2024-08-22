2548aab837c3a09f8025e18609f27afe612f9bc4
Angel Gabriel Manzano Contreras <angel.manzano1326@alumnos.udg.mx>
2024-08-21 23:55:17 -0600

    feat(skill): icons made smaller when screen so

--- src/components/skill/index.tsx
+++ src/components/skill/index.tsx
@@ -7,16 +7,15 @@ import styles from './skill.module.css';
 export default function index({ img, name } : ISkill) {
   return (
     <article className={ styles[`skill`] }>
-      <figure className={ styles[`skill--icon`] }>
+      <figure className={ styles[`skill__icon`] }>
         <Image 
           src={ img } 
           alt={ name } 
-          width={ 100 }
-          height={ 100 }
+          fill={ true }
           />
       </figure>
 
-      <p className={ styles[`skill--title`] }>{ name }</p>
+      <p className={ styles[`skill__title`] }>{ name }</p>
     </article>
   )
 }
